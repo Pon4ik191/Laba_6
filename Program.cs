28f66e26c46c8c4d319fcf390eb15dfb0594cee3
Artem Bikmaiev <artemasik.b@gmail.com>
2023-04-24 18:10:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Laba_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            File.WriteAllText("input.txt", "5\n2\n9\n1\n8\n532\n23\n94\n15\n82\n");
+
+            string[] lines = File.ReadAllLines("input.txt");
+
+            var sortedLines = lines.OrderBy(line => int.Parse(line));
+
+            File.WriteAllLines("output.txt", sortedLines);
+
+            Console.WriteLine("Файл був відсортований та записаний у файл output.txt");
+        }
+    }
+}
